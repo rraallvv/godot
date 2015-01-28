9b558765192b213081ef187f19c898764aaf312c
Rhody Lugo <rhodylugo@gmail.com>
2015-01-28 06:10:01 -0430

    Add SIMD to the class Quat

--- core/math/quat.cpp
+++ core/math/quat.cpp
@@ -29,6 +29,19 @@
 #include "quat.h"
 #include "print_string.h"
 
+#if defined(USE_SSE)
+
+#define vQInv (_mm_set_ps(+0.0f, -0.0f, -0.0f, -0.0f))
+#define vPPPM (_mm_set_ps(-0.0f, +0.0f, +0.0f, +0.0f))
+#define vOnes (_mm_set_ps(1.0f, 1.0f, 1.0f, 1.0f))
+
+#elif defined(USE_NEON)
+
+const SimdFloat4 ATTRIBUTE_ALIGNED16(vQInv) = {-0.0f, -0.0f, -0.0f, +0.0f};
+const SimdFloat4 ATTRIBUTE_ALIGNED16(vPPPM) = {+0.0f, +0.0f, +0.0f, -0.0f};
+
+#endif
+
 void Quat::set_euler(const Vector3& p_euler) {
 	real_t half_yaw = p_euler.x * 0.5;  
 	real_t half_pitch = p_euler.y * 0.5;  
@@ -47,10 +60,82 @@ void Quat::set_euler(const Vector3& p_euler) {
 
 void Quat::operator*=(const Quat& q) {
 
+#if defined (USE_SSE)
+	__m128 vQ2 = q.vec128;
+	
+	__m128 A1 = pshufd_ps(vec128, SHUFFLE(0,1,2,0));
+	__m128 B1 = pshufd_ps(vQ2, SHUFFLE(3,3,3,0));
+	
+	A1 = A1 * B1;
+	
+	__m128 A2 = pshufd_ps(vec128, SHUFFLE(1,2,0,1));
+	__m128 B2 = pshufd_ps(vQ2, SHUFFLE(2,0,1,1));
+	
+	A2 = A2 * B2;
+	
+	B1 = pshufd_ps(vec128, SHUFFLE(2,0,1,2));
+	B2 = pshufd_ps(vQ2, SHUFFLE(1,2,0,2));
+	
+	B1 = B1 * B2;	//	A3 *= B3
+	
+	vec128 = splat_ps(vec128, 3);	//	A0
+	vec128 = vec128 * vQ2;	//	A0 * B0
+	
+	A1 = A1 + A2;	//	AB12
+	vec128 = vec128 - B1;	//	AB03 = AB0 - AB3 
+	A1 = _mm_xor_ps(A1, vPPPM);	//	change sign of the last element
+	vec128 = vec128+ A1;	//	AB03 + AB12
+
+#elif defined(USE_NEON)
+
+	float32x4_t vQ1 = vec128;
+	float32x4_t vQ2 = q.vec128;
+	float32x4_t A0, A1, B1, A2, B2, A3, B3;
+	float32x2_t vQ1zx, vQ2wx, vQ1yz, vQ2zx, vQ2yz, vQ2xz;
+	
+	{
+	float32x2x2_t tmp;
+	tmp = vtrn_f32( vget_high_f32(vQ1), vget_low_f32(vQ1) );       // {z x}, {w y}
+	vQ1zx = tmp.val[0];
+
+	tmp = vtrn_f32( vget_high_f32(vQ2), vget_low_f32(vQ2) );       // {z x}, {w y}
+	vQ2zx = tmp.val[0];
+	}
+	vQ2wx = vext_f32(vget_high_f32(vQ2), vget_low_f32(vQ2), 1); 
+
+	vQ1yz = vext_f32(vget_low_f32(vQ1), vget_high_f32(vQ1), 1);
+
+	vQ2yz = vext_f32(vget_low_f32(vQ2), vget_high_f32(vQ2), 1);
+	vQ2xz = vext_f32(vQ2zx, vQ2zx, 1);
+
+	A1 = vcombine_f32(vget_low_f32(vQ1), vQ1zx);                    // X Y  z x 
+	B1 = vcombine_f32(vdup_lane_f32(vget_high_f32(vQ2), 1), vQ2wx); // W W  W X 
+
+	A2 = vcombine_f32(vQ1yz, vget_low_f32(vQ1));
+	B2 = vcombine_f32(vQ2zx, vdup_lane_f32(vget_low_f32(vQ2), 1));
+
+	A3 = vcombine_f32(vQ1zx, vQ1yz);        // Z X Y Z
+	B3 = vcombine_f32(vQ2yz, vQ2xz);        // Y Z x z
+
+	A1 = vmulq_f32(A1, B1);
+	A2 = vmulq_f32(A2, B2);
+	A3 = vmulq_f32(A3, B3);	//	A3 *= B3
+	A0 = vmulq_lane_f32(vQ2, vget_high_f32(vQ1), 1); //	A0 * B0
+
+	A1 = vaddq_f32(A1, A2);	//	AB12 = AB1 + AB2
+	A0 = vsubq_f32(A0, A3);	//	AB03 = AB0 - AB3 
+	
+	//	change the sign of the last element
+	A1 = (SimdFloat4)veorq_s32((int32x4_t)A1, (int32x4_t)vPPPM);	
+	A0 = vaddq_f32(A0, A1);	//	AB03 + AB12
+	
+	vec128 = A0;
+#else
 	set(w * q.x+x * q.w+y * q.z - z * q.y,
 		w * q.y+y * q.w+z * q.x - x * q.z,
 		w * q.z+z * q.w+x * q.y - y * q.x,
 		w * q.w - x * q.x - y * q.y - z * q.z);
+#endif
 }
 
 Quat Quat::operator*(const Quat& q) const {
@@ -69,7 +154,23 @@ real_t Quat::length() const {
 }
 
 void Quat::normalize() {
+#if defined (USE_SSE)
+	__m128	vd;
+	
+	vd = _mm_mul_ps(vec128, vec128);
+	
+	__m128 t = _mm_movehl_ps(vd, vd);
+	vd = _mm_add_ps(vd, t);
+	t = _mm_shuffle_ps(vd, vd, 0x55);
+	vd = _mm_add_ss(vd, t);
+
+	vd = _mm_sqrt_ss(vd);
+	vd = _mm_div_ss(vOnes, vd);
+	vd = pshufd_ps(vd, 0); // splat
+	vec128 = _mm_mul_ps(vec128, vd);
+#else
 	*this /= length();
+#endif
 }
 
 
@@ -78,7 +179,13 @@ Quat Quat::normalized() const {
 } 
 
 Quat Quat::inverse() const {
+#if defined (USE_SSE)
+	return Quat(_mm_xor_ps(vec128, vQInv));
+#elif defined(USE_NEON)
+	return Quat((SimdFloat4)veorq_s32((int32x4_t)vec128, (int32x4_t)vQInv));
+#else	
 	return Quat( -x, -y, -z, w );
+#endif
 }
 
 
--- core/math/quat.h
+++ core/math/quat.h
@@ -40,7 +40,22 @@
 class Quat{
 public:
 
+#if defined (USE_SSE) || defined (USE_NEON)
+	union {
+
+		struct {
+			real_t x;
+			real_t y;
+			real_t z;
+			real_t w;
+		};		
+
+		SimdFloat4 vec128;
+
+	};
+#else
 	real_t x,y,z,w;
+#endif
 
 	_FORCE_INLINE_ real_t length_squared() const;
 	real_t length() const;
@@ -128,12 +143,52 @@ public:
 
 	inline Quat() {x=y=z=0; w=1; }
 	
+#if defined(USE_SSE) || defined(USE_NEON)
+	// Set Vector 
+	_FORCE_INLINE_ Quat(const SimdFloat4 vec)
+	{
+		vec128 = vec;
+	}
+
+	// Copy constructor
+	_FORCE_INLINE_ Quat(const Quat& rhs)
+	{
+		vec128 = rhs.vec128;
+	}
+
+	// Assignment Operator
+	_FORCE_INLINE_ Quat& operator=(const Quat& v)
+	{
+		vec128 = v.vec128;
+		
+		return *this;
+	}
+	
+#endif
 
 };
 
 
 real_t Quat::dot(const Quat& q) const {
+#if defined (USE_SSE)
+	__m128	vd;
+	
+	vd = _mm_mul_ps(vec128, q.vec128);
+	
+	__m128 t = _mm_movehl_ps(vd, vd);
+	vd = _mm_add_ps(vd, t);
+	t = _mm_shuffle_ps(vd, vd, 0x55);
+	vd = _mm_add_ss(vd, t);
+	
+	return _mm_cvtss_f32(vd);
+#elif defined(USE_NEON)
+	float32x4_t vd = vmulq_f32(vec128, q.vec128);
+	float32x2_t x = vpadd_f32(vget_low_f32(vd), vget_high_f32(vd));
+	x = vpadd_f32(x, x);
+	return vget_lane_f32(x, 0);
+#else
 	return x * q.x+y * q.y+z * q.z+w * q.w;
+#endif
 }
 
 real_t Quat::length_squared() const {
@@ -141,15 +196,35 @@ real_t Quat::length_squared() const {
 }
 
 void Quat::operator+=(const Quat& q) {
+#if defined (USE_SSE)
+	vec128 = _mm_add_ps(vec128, q.vec128);
+#elif defined(USE_NEON)
+	vec128 = vaddq_f32(vec128, q.vec128);
+#else	
 	x += q.x; y += q.y; z += q.z; w += q.w;	
+#endif
 }
 
 void Quat::operator-=(const Quat& q) {
+#if defined (USE_SSE)
+	vec128 = _mm_sub_ps(vec128, q.vec128);
+#elif defined(USE_NEON)
+	vec128 = vsubq_f32(vec128, q.vec128);
+#else	
 	x -= q.x; y -= q.y; z -= q.z; w -= q.w;
+#endif
 }
 
 void Quat::operator*=(const real_t& s) {
+#if defined (USE_SSE)
+	__m128	vs = _mm_load_ss(&s);	//	(S 0 0 0)
+	vs = pshufd_ps(vs, 0);	//	(S S S S)
+	vec128 = _mm_mul_ps(vec128, vs);
+#elif defined(USE_NEON)
+	vec128 = vmulq_n_f32(vec128, s);
+#else
 	x *= s; y *= s; z *= s; w *= s;
+#endif
 }
 
 
@@ -159,22 +234,49 @@ void Quat::operator/=(const real_t& s) {
 }
 
 Quat Quat::operator+(const Quat& q2) const {
+#if defined (USE_SSE)
+	return Quat(_mm_add_ps(vec128, q2.vec128));
+#elif defined(USE_NEON)
+	return Quat(vaddq_f32(vec128, q2.vec128));
+#else	
 	const Quat& q1 = *this;
 	return Quat( q1.x+q2.x, q1.y+q2.y, q1.z+q2.z, q1.w+q2.w );
+#endif
 }
 
 Quat Quat::operator-(const Quat& q2) const {
+#if defined (USE_SSE)
+	return Quat(_mm_sub_ps(vec128, q2.vec128));
+#elif defined(USE_NEON)
+	return Quat(vsubq_f32(vec128, q2.vec128));
+#else	
 	const Quat& q1 = *this;
 	return Quat( q1.x-q2.x, q1.y-q2.y, q1.z-q2.z, q1.w-q2.w);
+#endif
 }
 
 Quat Quat::operator-() const {
+#if defined (USE_SSE)
+	return Quat(_mm_xor_ps(vec128, vMzeroMask));
+#elif defined(USE_NEON)
+	return Quat((SimdFloat4)veorq_s32((int32x4_t)vec128, (int32x4_t)vMzeroMask) );
+#else	
 	const Quat& q2 = *this;
 	return Quat( -q2.x, -q2.y,  -q2.z,  -q2.w);
+#endif
 }
 
 Quat Quat::operator*(const real_t& s) const {
+#if defined (USE_SSE)
+	__m128	vs = _mm_load_ss(&s);	//	(S 0 0 0)
+	vs = pshufd_ps(vs, 0x00);	//	(S S S S)
+		
+	return Quat(_mm_mul_ps(vec128, vs));
+#elif defined(USE_NEON)
+	return Quat(vmulq_n_f32(vec128, s));
+#else
 	return Quat(x * s, y * s, z * s, w * s);
+#endif
 }
 
 Quat Quat::operator/(const real_t& s) const {
@@ -183,13 +285,16 @@ Quat Quat::operator/(const real_t& s) const {
 
 
 bool Quat::operator==(const Quat& p_quat) const {
-
-	return x==p_quat.x && y==p_quat.y && z==p_quat.z && w==p_quat.w;
+#if defined (USE_SSE)
+		return (0xf == _mm_movemask_ps((__m128)_mm_cmpeq_ps(vec128, p_quat.vec128)));
+#else
+        return (x==p_quat.x && y==p_quat.y && z==p_quat.z && w==p_quat.w);
+#endif
 }
 
 bool Quat::operator!=(const Quat& p_quat) const {
 
-	return x!=p_quat.x || y!=p_quat.y || z!=p_quat.z || w!=p_quat.w;
+	return !(*this == p_quat);
 }
 
 
