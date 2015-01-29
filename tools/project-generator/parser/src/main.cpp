ef69ce436db08e90435f0bb1a4aa54c65ef5d536
Rhody Lugo <rhodylugo@gmail.com>
2015-01-28 21:40:15 -0430

    PG: Update copyright note.

--- tools/project-generator/parser/src/main.cpp
+++ tools/project-generator/parser/src/main.cpp
@@ -1,7 +1,7 @@
 /*************************************************************************/
 /*  main.cpp                                                             */
 /*************************************************************************/
-/* Copyright (c) 2014 Rhody Lugo.                                        */
+/* Copyright (c) 2014-2015 Rhody Lugo.                                   */
 /*                                                                       */
 /* Permission is hereby granted, free of charge, to any person obtaining */
 /* a copy of this software and associated documentation files (the       */
