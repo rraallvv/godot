8b227e02f7bee72dadca75b326bfca44fb431b17
Rhody Lugo <rhodylugo@gmail.com>
2015-02-07 04:01:26 -0430

    physics 3d script test: create the container and two 3x3x3 matrices of bodies

--- demos/performance/physics_3d_script/script_test.gd
+++ demos/performance/physics_3d_script/script_test.gd
@@ -3,15 +3,71 @@ extends Spatial
 
 # This demo is an example of creating 3d body physics in GDScript.
 
-const CUBE_COUNT = 10
-const WIDTH = 10
-	
 var cubes=[]
 var shape
 
-class Cube:
+
+class VisibleBody:
 	extends TestCube
 	var body = RID()
+
+
+class TrasparentBody:
+	extends Spatial
+	var body = RID()
+
+
+func create_container():
+	create_wall(Vector3(-10,0,0), Vector3(1,11,11))
+	create_wall(Vector3(10,0,0), Vector3(1,11,11))
+	create_wall(Vector3(0,-10,0), Vector3(11,1,11))
+	create_wall(Vector3(0,10,0), Vector3(11,1,11))
+	create_wall(Vector3(0,0,-10), Vector3(11,11,1), true)
+	create_wall(Vector3(0,0,10), Vector3(11,11,1), true)
+
+
+func create_wall(origin, scale, transparent=false):
+	var cube = RID()
+	if transparent:
+		cube = TrasparentBody.new()
+	else:
+		cube = VisibleBody.new()
+	cube.body = PhysicsServer.body_create(PhysicsServer.BODY_MODE_STATIC)
+	PhysicsServer.body_set_space(cube.body,get_world().get_space())
+	PhysicsServer.body_add_shape(cube.body,shape)
+	
+	var trans = Transform()
+	trans.origin = origin
+	trans.basis = Matrix3().scaled(Vector3(1,1,1))
+	cube.set_transform(trans)
+		
+	PhysicsServer.body_set_state(cube.body,PhysicsServer.BODY_STATE_TRANSFORM,trans)
+	PhysicsServer.body_set_param(cube.body,PhysicsServer.BODY_PARAM_BOUNCE,0.0)
+	#PhysicsServer.body_set_param(cube.body,PhysicsServer.BODY_PARAM_MASS,1.0)
+	cubes.append(cube)
+	add_child(cube)
+
+
+func create_matrix(position):
+	var separation = 3.0
+	for i in range(3):
+		for j in range(3):
+			for k in range(3):
+				var cube = VisibleBody.new()
+				cube.body = PhysicsServer.body_create(PhysicsServer.BODY_MODE_RIGID)
+				PhysicsServer.body_set_space(cube.body,get_world().get_space())
+				PhysicsServer.body_add_shape(cube.body,shape)
+				
+				var trans = Transform()
+				trans.origin = separation * (Vector3(-1,-1,-1) + Vector3(i, j, k)) + position
+				
+				cube.set_transform(trans)
+				PhysicsServer.body_set_state(cube.body,PhysicsServer.BODY_STATE_TRANSFORM,trans)
+				PhysicsServer.body_set_param(cube.body,PhysicsServer.BODY_PARAM_BOUNCE,0.0)
+				PhysicsServer.body_set_param(cube.body,PhysicsServer.BODY_PARAM_MASS,0.01)
+				
+				cubes.append(cube)
+				add_child(cube)
 
 
 func _process(delta):
@@ -23,39 +79,9 @@ func _process(delta):
 func _ready():
 	shape = PhysicsServer.shape_create(PhysicsServer.SHAPE_BOX)
 	PhysicsServer.shape_set_data(shape,Vector3(1,1,1)) # half extents
-	
-	for i in range(CUBE_COUNT):
-		var cube = Cube.new()
-		cube.body = PhysicsServer.body_create(PhysicsServer.BODY_MODE_RIGID)
-		PhysicsServer.body_set_space(cube.body,get_world().get_space())
-		PhysicsServer.body_add_shape(cube.body,shape)
-		
-		var trans = Transform()
-		trans.origin=Vector3(rand_range(-WIDTH,WIDTH),10,rand_range(-WIDTH,WIDTH))
-		#if randf() > 0.5:
-		#	trans = trans.rotated(Vector3(1,1,1), 45)
-		
-		cube.set_transform(trans)
-		PhysicsServer.body_set_state(cube.body,PhysicsServer.BODY_STATE_TRANSFORM,trans)
-		PhysicsServer.body_set_param(cube.body,PhysicsServer.BODY_PARAM_BOUNCE,1.0)
-		
-		"""
-		if randf() > 0.5:
-			PhysicsServer.body_set_param(cube.body,PhysicsServer.BODY_PARAM_MASS,0)
-		else:
-			PhysicsServer.body_set_param(cube.body,PhysicsServer.BODY_PARAM_MASS,0.01)
-		"""
-		
-		"""
-		if randf() > 0.5:
-			PhysicsServer.body_set_param(cube.body,PhysicsServer.BODY_PARAM_FRICTION,0.0)
-		else:
-			PhysicsServer.body_set_param(cube.body,PhysicsServer.BODY_PARAM_FRICTION,1.0)
-		"""
-		
-		cubes.append(cube)
-		add_child(cube)
-		
+	create_container()
+	create_matrix(Vector3(-4.5,0,0))
+	create_matrix(Vector3(4.5,0,0))
 	set_process(true)
 
 
